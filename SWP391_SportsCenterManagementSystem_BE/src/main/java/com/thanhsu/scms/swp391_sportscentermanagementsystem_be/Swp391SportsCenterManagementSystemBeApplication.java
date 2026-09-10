d487e25898cf941cda82a983e7bf0bb6575a63d2
thanhsu263 <thanhsu.tb.cl.tg.vn@gmail.com>
2026-09-10 10:02:21 +0700

    add FE and BE

---
+++ SWP391_SportsCenterManagementSystem_BE/src/main/java/com/thanhsu/scms/swp391_sportscentermanagementsystem_be/Swp391SportsCenterManagementSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.thanhsu.scms.swp391_sportscentermanagementsystem_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391SportsCenterManagementSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp391SportsCenterManagementSystemBeApplication.class, args);
+    }
+
+}
---
+++ SWP391_SportsCenterManagementSystem_BE/src/test/java/com/thanhsu/scms/swp391_sportscentermanagementsystem_be/Swp391SportsCenterManagementSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhsu.scms.swp391_sportscentermanagementsystem_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp391SportsCenterManagementSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
